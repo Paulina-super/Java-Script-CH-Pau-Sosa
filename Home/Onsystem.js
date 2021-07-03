95dc1772b681f02a3cc8042087ad1a1f97c41b79
Paulina-super <56806110+Paulina-super@users.noreply.github.com>
2021-07-02 22:40:37 -0300

    Delete Home directory

--- Home/Onsystem.js
+++
@@ -1,7 +0,0 @@
-const Init = () => { LoadDataBaseFromJSON(); }
-const LoadDataBaseFromJSON = () => {
-    $.getJSON("productos.json", function(productos) {
-        let productosJASON = ConvertorString(productos)
-        SetLocal(LocalStorageGlobalVariables.Todos_Productos, productosJASON);
-    });
-};
